ac65f48abd4dcda73b3f9f099bd1d36098ff5c7a
Ian Pollack <ianjpollack@gmail.com>
2018-03-31 01:12:21 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -39,24 +39,4 @@ def nyc_pigeon_organizer(data)
     end
   end
 
-       binding.pry
-
-        if birdsArray.include?(bird)
-#          binding.pry
-        else
-          birdsArray << bird
-#          binding.pry
-        end
-      end
-
-
-#  binding.pry
-  i = 0
-  until i == birdsArray.length
-    newHash[birdsArray[i]] = {}
-    newHash[birdsArray[i]][:color] = []
-    newHash[birdsArray[i]][:gender] = []
-    newHash[birdsArray[i]][:lives] = []
-    i += 1
-  end
-binding.pry
+  
