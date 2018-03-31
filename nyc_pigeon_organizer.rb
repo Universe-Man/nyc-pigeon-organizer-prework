1cc99c09133f2a32a8016d04131e5fc07f779bc3
Ian Pollack <ianjpollack@gmail.com>
2018-03-31 01:23:05 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,44 +2,23 @@ require "pry"
 
 def nyc_pigeon_organizer(data)
   birdsArray = []
-
   newHash = {}
-#  binding.pry
   data.each do |category, hash|
-    if category == :color
-      hash.each do |key, array|
-        array.each do |bird|
-
-
-          if data[:color].values.include?(bird)
-            binding.pry
-            newHash[bird][:color] = [key.to_s]
-          end
+    hash.each do |key, array|
+      array.each do |bird|
+        if birdsArray.include?(bird)
+        else
+          birdsArray << bird
         end
       end
     end
   end
-  data.each do |category, hash|
-    if category == :gender
-      hash.each do |key, array|
-        array.each do |bird|
-          if data[:color].values.include?(bird)
-            newHash[bird][:gender] = [key.to_s]
-          end
-        end
-      end
-    end
+  i = 0
+  until i == birdsArray.length
+    newHash[birdsArray[i]] = {}
+    newHash[birdsArray[i]][:color] = []
+    newHash[birdsArray[i]][:gender] = []
+    newHash[birdsArray[i]][:lives] = []
+    i += 1
   end
-  data.each do |category, hash|
-    if category == :lives
-      hash.each do |key, array|
-        array.each do |bird|
-          if data[:color].values.include?(bird)
-            newHash[bird][:lives] = [key.to_s]
-          end
-        end
-      end
-    end
-  end
-  binding.pry
 end
