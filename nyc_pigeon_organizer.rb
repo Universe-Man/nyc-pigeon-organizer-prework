ac76a33fe9f294c81fa83c354397c5229d97d4f7
Ian Pollack <ianjpollack@gmail.com>
2018-03-31 01:09:31 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,14 +2,45 @@ require "pry"
 
 def nyc_pigeon_organizer(data)
   birdsArray = []
+
   newHash = {}
 #  binding.pry
   data.each do |category, hash|
-#    binding.pry
-    hash.each do |key, array|
-#      binding.pry
-      array.each do |bird|
-#        binding.pry
+    if category == :color
+      hash.each do |key, array|
+        array.each do |bird|
+          if key.values.include?(bird)
+            newHash[bird][:color] = [key.to_s]
+          end
+        end
+      end
+    end
+  end
+  data.each do |category, hash|
+    if category == :gender
+      hash.each do |key, array|
+        array.each do |bird|
+          if key.values.include?(bird)
+            newHash[bird][:gender] = [key.to_s]
+          end
+        end
+      end
+    end
+  end
+  data.each do |category, hash|
+    if category == :lives
+      hash.each do |key, array|
+        array.each do |bird|
+          if key.values.include?(bird)
+            newHash[bird][:lives] = [key.to_s]
+          end
+        end
+      end
+    end
+  end
+
+       binding.pry
+
         if birdsArray.include?(bird)
 #          binding.pry
         else
